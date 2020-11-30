d8eb913afc090b3eb04617309720153aacaecb23
ad-tra <aew31gcmuy@gmail.com>
2020-11-29 23:03:18 -0500

    Bare Bone Website

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,36 +1,43 @@
-function getBottomP(j){
-    return  document.querySelectorAll('.picture-container p')[j].getBoundingClientRect().y 
+function myFunction(x) {
+   if (x.matches) { // If media query matches
+      document.querySelector('h1').innerHTML = document.querySelector('h1').textContent 
+   } else{
+      document.querySelector('h1').innerHTML = "Art through Years<br> of Patience"
+   }
+    
+
  }
- function getTopP(j){
-    return  document.querySelectorAll('.picture-container p')[j].getBoundingClientRect().y + document.querySelectorAll('.picture-container p')[j].getBoundingClientRect().height
- }
+ 
+ var x = window.matchMedia("(max-width: 1500px)")
+ myFunction(x) // Call listener function at run time
+ x.addListener(myFunction) // Attach listener function on state changes
 
  var j = 0;
- window.addEventListener('scroll', ()=>{
+//  window.addEventListener('scroll', ()=>{
      
      
-     try{
-     var p = getBottomP(j)
-    } catch(e){}
+//      try{
+//      var p = getBottomP(j)
+//     } catch(e){}
      
-    //check to see if year reashed p
-    if(p< document.querySelector('.drawing-year').getBoundingClientRect().height +200){
-     gsap.to('.drawing-year' ,{opacity: 0.8, duration: 5, ease: 'slow'})    
- }
-     if (p<0 || p > document.querySelector('.drawing-year').getBoundingClientRect().height +199){
-         
+//     //check to see if year reashed p
+//     if(p< document.querySelector('.drawing-year').getBoundingClientRect().height +200){
+//      gsap.to('.drawing-year' ,{opacity: 0, duration: 0.5, ease: 'slow'})    
+//  }
+//      if (p<0 || p > document.querySelector('.drawing-year').getBoundingClientRect().height +199){
+//         gsap.to('.drawing-year' ,{opacity: 0.8, duration: 1, ease: 'slow'})   
            
-         if(p<0)j++
-     }
+//          if(p<0)j++
+//      }
          
      
-     let temp = j-1
-     //check to change tracker if previous 
-     if(j != 0 && getBottomP(temp) >0 ){
-         j--
-     }
+//      let temp = j-1
+//      //check to change tracker if previous 
+//      if(j != 0 && getBottomP(temp) >0 ){
+//          j--
+//      }
         
      
-     console.log("j: "+ j +" p: "+ parseInt(p))
+//      console.log("j: "+ j +" p: "+ parseInt(p))
     
- })
+//  })
